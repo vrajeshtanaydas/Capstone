afd130fd298fbd058b285b676423d83161220efe
cherreline <ch485@nau.edu>
2015-04-02 16:52:07 -0700

    Pipeline comments

--- Pipeline.py
+++ Pipeline.py
@@ -1,3 +1,21 @@
+##############################################
+#
+#The pipeline requires four arguments:
+#	inputFile - the name/path of the ISG data file
+#	outputFile - the name used to create the output files
+#	treeFile - the name/path of the phylogenetic tree file
+#	referenceFile - the name/path of the FASTA file used as the 
+#		reference genome in the ISG data
+#
+#The pipeline creates two files, <outputFile>.txt and <outputFile>.fasta.
+#	<outputFile>.txt contains all the sequences and the genomes/groups 
+#		that they differentiate in a human-readable format.
+#	<outputFile>.fasta contains all the sequences in FASTA format.
+#
+#
+##############################################
+
+
 # since this is part of the pipeline, we need sys module
 import sys
 import ISGDataPuller
@@ -34,7 +52,7 @@ ISGData = ISGDataPuller.main(inputFile, treeTable)
 
 sequences = SequenceFinder.main(treeTable,ISGData)
 
-SequenceToFasta.main(sequences, referenceFile)
+SequenceToFasta.main(sequences, referenceFile, outputFile)
 
 #for sequence in sequences:
     #print(sequence.getSNPList())
