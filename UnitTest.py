a5fdd416df3c4ae43f2dc5f36cbc4a59ba7665b2
Ian Humphrey <irh9@nau.edu>
2015-04-20 20:24:04 -0700

    updated unit test with fixes to TestTreeMakerMethods test class

--- UnitTest.py
+++ UnitTest.py
@@ -1,5 +1,4 @@
 import unittest
-import Pipeline
 import TreeMaker
 import ISGDataPuller
 import SequenceFinder
@@ -7,18 +6,48 @@ import EntropyCalculator
 import SequenceDifferentiator
 import SequenceToFasta
 
+
+# Test the TreeTableMaker class
 class TestTreeMakerMethods(unittest.TestCase):
 
     def setUp(self):
-        fo = open('testTreeMaker.txt', 'w')
+        self.fname = 'testTreeMaker.txt'
+        self.fo = open(self.fname, 'w+')
+
+
+    # Test the normal operation of preprocess()
+    def test_preprocess_normal(self):
+        self.fo.write('(a:0.5,b:0.7,(c:0.0002,d:0.1,(e:0.99)))')
+        # close the file since TreeTable is going to open it
+        self.fo.close()
+        processed = TreeMaker.TreeTable(self.fname).processed
+
+        self.assertEqual(processed, ['(', 'a', 'b', '(', 'c', 'd', '(', 'e',
+            ')', ')', ')'])
+
+
+    # Boundary condition test
+    def test_preprocess_two_genomes(self):
+        self.fo.write('(a:0.5,b:0.7)')
+        # close the file since TreeTable is going to open it
+        self.fo.close()
+        processed = TreeMaker.TreeTable(self.fname).processed
+
+        self.assertEqual(processed, ['(', 'a', 'b', ')'])
     
-    def test_preprocess_boundary(self):
-        fo.write('(a:0.5,b:0.7)')
-        self.assertEqual(TreeMaker.TreeTable(fo), ['(', 'a', 'b', ')'])
-    
+
+    # Test bad characters in file to preprocess
     def test_preprocess_wrong_1(self):
-        fo.write('{a:0.5,b:0.7}')
-        self.assertRaises(IndexError, TreeMaker.TreeTable(), fo)
-    
+        self.fo.write('{a:0.5,b:0.7}')
+        self.fo.close()
+
+        with self.assertRaises(TreeMaker.NewickFileError) as cm:
+            TreeMaker.TreeTable(self.fname)
+
+
+
     def tearDown(self):
-        fo.close()
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
